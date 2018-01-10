be36367a09f2323d602f1fe003f07fb3254bcc81
RHUL-James-King <james.king.2015@live.rhul.ac.uk>
2018-01-10 15:54:40 +0000

    Added functionality to XYTarget system

---
+++ test/XYTargetTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+public class XYTargetTest {
+
+    public boolean doXYExist(int[] array, int target) {
+        for(int i = 0 ; i < array.length; i++) {
+
+        }
+    }
+}
